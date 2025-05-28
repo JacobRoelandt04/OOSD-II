280d3a9d52a18e210abedb6651c91bd2979a3353
Jacob Roelandt <jacob.roelandt@student.hogent.be>
2025-05-28 11:13:47 +0200

    Add files via upload

---
+++ H2/Exercises/H02_Rekening_Start/src/persistentie/BeheerskostenMapper.java
@@ -0,0 +1,26 @@
+package persistentie;
+
+public class BeheerskostenMapper
+{
+
+/* Terug te geven objecten:
+	SpaarRekening s1, s2;
+	ZichtRekening z1, z2;
+	Kluis k1, k2;
+	
+	SpaarRekening.setAangroeiIntrest(2);
+	
+	s1 = new SpaarRekening(101L, "Toon");
+	s2 = new SpaarRekening(202L, "Sebastien");
+	z1 = new ZichtRekening(303L, "Stef", -1000);
+	z2 = new ZichtRekening(404L, "Michiel", -2000);
+	k1 = new Kluis("Dylan", 58);
+	k2 = new Kluis("Lander", 59);
+	
+	s1.stortOp(100);
+	s2.stortOp(200);
+	z1.stortOp(300);
+	z2.stortOp(400);
+*/
+
+}
