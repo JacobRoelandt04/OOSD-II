aa85339963a882958b2e562c395a096dfe8713e9
Jacob Roelandt <jacob.roelandt@student.hogent.be>
2025-06-04 10:53:35 +0200

    Update Motor.java

--- ex/src/domein/Motor.java
+++ ex/src/domein/Motor.java
@@ -1,5 +1,22 @@
 package domein;
 
-public class Motor {
+public class Motor extends Voertuig{
+	private int cilinderinhoud;
+	
+	public Motor(String merk, int bouwjaar, double prijs, int cilinderinhoud) {
+		super(merk, bouwjaar, prijs);
+		this.cilinderinhoud = cilinderinhoud;
+	}
 
+
+	@Override
+	public double berekenBelasting() {
+		return this.cilinderinhoud * 0.15;
+	}
+
+	@Override
+	String getVoertuigType() {
+		return getClass().getSimpleName();
+	}
+	
 }
