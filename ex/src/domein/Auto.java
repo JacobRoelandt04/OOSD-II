bb3bd8d408f80f3111cec8f0aa520de2021917cf
Jacob Roelandt <jacob.roelandt@student.hogent.be>
2025-06-04 10:53:25 +0200

    Update Auto.java

--- ex/src/domein/Auto.java
+++ ex/src/domein/Auto.java
@@ -1,5 +1,20 @@
 package domein;
 
-public class Auto {
+public class Auto extends Voertuig{
+	private int aantalDeuren;
+	
+	public Auto(String merk, int bouwjaar, double prijs, int aantalDeuren) {
+		super(merk, bouwjaar, prijs);
+		this.aantalDeuren = aantalDeuren;
+	}
 
+	@Override
+	public double berekenBelasting() {
+		return super.getBouwjaar() >= 2020 ? super.prijs * 0.05 : super.prijs * 0.03;
+	}
+
+	@Override
+	String getVoertuigType() {
+		return getClass().getSimpleName();
+	}
 }
